da9bf6f4462d204b736584fcd5c31a6025d97018
rabiilfarakh <rabiilfarakh2816@gmail.com>
2024-09-30 14:58:40 +0100

    first commit

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Employee {
+    private String name;
+    private String email;
+    private String phone;
+    private String departement;
+    private String poste;
+}
